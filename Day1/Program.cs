7e211e69f5bf87698404230b958ef58ac5d6d312
Estela Rubio <estelarub@gmail.com>
2024-12-01 21:22:13 +0100

    Extract Utils as a library

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,6 +1,5 @@
-﻿using Day1;
-
-var (column1, column2) = (await Utils.ReadInputFileAsync()).ToColumns();
+﻿
+var (column1, column2) = (await Utils.Utils.ReadInputFileAsync("../../../input.txt")).ToColumns();
 
 var finalResult = 0;
 var columnsLenght = column1.Count;
---
+++ Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿namespace Utils;
+
+public static class Utils
+{
+    public static async Task<string> ReadInputFileAsync(string path)
+    {
+        try
+        {
+            using StreamReader reader = new(path);
+            return await reader.ReadToEndAsync();
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+            throw new IOException();
+        }
+    }
+}
