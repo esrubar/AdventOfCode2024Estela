1d027baa501101ceffa90f49ff73115a71ddf5f8
Estela Rubio <estela.rubio@colba.es>
2024-12-04 00:27:20 +0100

    Day 3 - Part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+var text = await Utils.Utils.ReadInputFileAsync("../../../input.txt");
+
+var mulMatches = Regex.Matches(text, @"\w*mul\(\d+,\d+\)");
+
+var result = 0;
+foreach (Match mulMatch in mulMatches)
+{
+    const string pattern = @"\d+";
+    var numbers = Regex.Matches(mulMatch.Value, pattern);
+
+    var multiplicationResult = Convert.ToInt16(numbers[0].Value) * Convert.ToInt16(numbers[1].Value);
+    result += multiplicationResult;
+}
+
+Console.WriteLine(result);
