d203219cd384ee0e08888002918d691f645e1118
Estela Rubio <binternet2019@gmail.com>
2024-12-11 23:32:50 +0100

    Day 7 - Part 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -10,47 +10,56 @@ foreach (var line in textList)
     numbers.RemoveAt(0);
     var firstNumber = Convert.ToInt64(allNumbers[0]);
 
-    var operadores = GenerateOperations(numbers.Count - 1);
+    var operations = GenerateOperations(numbers.Count - 1);
 
-    foreach (var operador in operadores)
+    foreach (var operador in operations)
     {
+        var operador2 = operador.Split([" "], StringSplitOptions.RemoveEmptyEntries).ToList();
         var expresionList = new List<object>() { numbers[0] };
-        for (int i = 0; i < operador.Length; i++)
+        for (int i = 0; i < operador2.Count; i++)
         {
-            expresionList.Add(operador[i]);
+            expresionList.Add(operador2[i]);
             expresionList.Add(numbers[i + 1]);
         }
 
         System.Data.DataTable dt = new();
-        var firstResult = $"{expresionList[0]}{expresionList[1]}{expresionList[2]}";
-        var operationResult = Convert.ToInt64(dt.Compute(firstResult, ""));
+        var result = expresionList[1].ToString() == "||"
+            ? $"{expresionList[0]}{expresionList[2]}"
+            : $"{expresionList[0]}{expresionList[1]}{expresionList[2]}";
+        var operationResult = Convert.ToInt64(dt.Compute(result, ""));
 
         for (int i = 3; i < expresionList.Count - 1; i += 2)
         {
-            var result = $"{operationResult}{expresionList[i]}{expresionList[i + 1]}.0";
-            operationResult = Convert.ToInt64(dt.Compute(result, ""));
+            var result2 = expresionList[i].ToString() == "||"
+            ? $"{operationResult}{expresionList[i + 1]}.0"
+            : $"{operationResult}{expresionList[i]}{expresionList[i + 1]}.0";
+            operationResult = Convert.ToInt64(dt.Compute(result2, ""));
         }
 
         if (operationResult == firstNumber)
         {
+            Console.WriteLine(firstNumber);
             sum += firstNumber;
             break;
         }
     }
 }
+Console.WriteLine("-------");
 Console.WriteLine(sum);
 return;
 
 static List<string> GenerateOperations(int quantity)
 {
-    if (quantity == 0) return [""]; // Caso base
+    if (quantity == 0) return [""];
     var previous = GenerateOperations(quantity - 1);
     var combinations = new List<string>();
 
     foreach (var operation in previous)
     {
-        combinations.Add(operation + "+");
-        combinations.Add(operation + "*");
+        combinations.Add(operation + " +");
+        combinations.Add(operation + " *");
+        combinations.Add(operation + " ||");
     }
+
     return combinations;
 }
